4567d7213fb9fce97c8de2638868dfcf81f5d1d7
Kristian Frost <frozt@MacBook-Pro-tilhrende-Kristian.local>
2026-02-10 22:24:54 +0100

    ready to move on

--- home/fish.nix
+++ home/fish.nix
@@ -6,6 +6,9 @@
       ll = "eza -la";
       lt = "eza --tree";
       cat = "bat";
+      update = "cd ~/nixos-config && nix flake update && git add flake.lock && git commit -m 'update packages' && git push && sudo nixos-rebuild switch --flake ~/nixos-config#x270";
+      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#x270";
+      rebuild-github = "sudo nixos-rebuild switch --flake github:kfrost89/nixos#x270 --refresh";
       ".." = "cd ..";
       "..." = "cd ../..";
     };
