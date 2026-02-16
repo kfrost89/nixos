8d729698331338628c348e39fae3ad4012ca74c8
Kristian Frost <frost.kristian@gmail.com>
2026-02-16 11:34:02 +0100

    updated fish

--- home/fish.nix
+++ home/fish.nix
@@ -23,7 +23,7 @@
       ll = "eza -la";
       lt = "eza --tree";
       cat = "bat";
-      update = "cd ~/nixos-config && nix --extra-experimental-features 'nix-command flakes' flake update && git add flake.lock && git commit -m 'update packages' && git push && sudo nixos-rebuild switch --flake ~/nixos-config#(hostname)";
+      update = "cd ~/nixos-config && nix --extra-experimental-features 'nix-command flakes' flake update && git add flake.lock && git diff --cached --quiet flake.lock && echo 'flake.lock already up to date, rebuilding...' || git commit -m 'update packages' && git push && sudo nixos-rebuild switch --flake ~/nixos-config#(hostname)";
       rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#(hostname)";
       rebuild-github = "sudo nixos-rebuild switch --flake github:kfrost89/nixos#(hostname) --refresh";
       preview-update = "cd ~/nixos-config && nix --extra-experimental-features 'nix-command flakes' flake update && nix --extra-experimental-features 'nix-command flakes' store diff-closures /run/current-system (nix --extra-experimental-features 'nix-command flakes' build ~/nixos-config#nixosConfigurations.(hostname).config.system.build.toplevel --print-out-paths --no-link)";
